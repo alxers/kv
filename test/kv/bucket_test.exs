0eaeeb95ef49af3fbf9d80e20c321a8b15a7fca8
Alexey Ershov <alex4rom@gmail.com>
2017-12-14 09:13:47 +0300

    Update test

--- test/kv/bucket_test.exs
+++ test/kv/bucket_test.exs
@@ -1,2 +1,11 @@
 defmodule KV.BucketTest do
+  use ExUnit.Case, async: true
+
+  test "stores values by key" do
+    {:ok, bucket} = start_supervised KV.Bucket
+    assert KV.Bucket.get(bucket, "milk") == nil
+
+    KV.Bucket.put(bucket, "milk", 3)
+    assert KV.Bucket.get(bucket, "milk") == 3
+  end
 end
