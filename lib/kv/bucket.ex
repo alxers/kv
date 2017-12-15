4f6329941f963da7bcac0bfff2d257fa5d88a91f
Alexey Ershov <alex4rom@gmail.com>
2017-12-15 09:33:21 +0300

    Add kv

---
+++ lib/kv/bucket.ex
@@ -0,0 +1,15 @@
+defmodule KV.Bucket do
+  use Agent
+
+  def start_link() do
+    Agent.start_link(fn -> %{} end)
+  end
+
+  def get(bucket, key) do
+    Agent.get(bucket, &Map.get(&1, key))
+  end
+
+  def put(bucket, key, value) do
+    Agent.update(bucket, &Map.put(&1, key, value))
+  end
+end
