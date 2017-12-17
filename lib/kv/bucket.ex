7809c453473f8263f989f4a59179d269d756676d
Alexey Ershov <alex4rom@gmail.com>
2017-12-17 11:29:32 +0300

    Update tests

--- lib/kv/bucket.ex
+++ lib/kv/bucket.ex
@@ -1,7 +1,7 @@
 defmodule KV.Bucket do
   use Agent
 
-  def start_link() do
+  def start_link(_opts) do
     Agent.start_link(fn -> %{} end)
   end
 
@@ -11,5 +11,9 @@ defmodule KV.Bucket do
 
   def put(bucket, key, value) do
     Agent.update(bucket, &Map.put(&1, key, value))
+  end
+
+  def delete(bucket, key) do
+    Agent.get_and_update(bucket, &Map.pop(&1, key))
   end
 end
--- test/kv/bucket_test.exs
+++ test/kv/bucket_test.exs
@@ -1,11 +1,23 @@
 defmodule KV.BucketTest do
   use ExUnit.Case, async: true
 
-  test "stores values by key" do
-    {:ok, bucket} = start_supervised KV.Bucket
+  setup do
+    {:ok, bucket} = start_supervised(KV.Bucket)
+    %{bucket: bucket}
+  end
+
+  test "stores values by key", %{bucket: bucket} do
     assert KV.Bucket.get(bucket, "milk") == nil
 
     KV.Bucket.put(bucket, "milk", 3)
     assert KV.Bucket.get(bucket, "milk") == 3
+  end
+
+  test "removes values by key", %{bucket: bucket} do
+    KV.Bucket.put(bucket, "milk", 1)
+    assert KV.Bucket.get(bucket, "milk") == 1
+
+    KV.Bucket.delete(bucket, "milk")
+    assert KV.Bucket.get(bucket, "milk") == nil
   end
 end
